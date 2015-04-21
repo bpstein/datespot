9484f8af090960f45fa37fa0f7c425583f391de5
Ben Stein <bumstain@gmail.com>
2015-04-22 00:37:15 +0100

    Added controllers for Discover and Favorites page and tab bar

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,20 +1,44 @@
-angular.module('starter.controllers', [])
+angular.module('datespot.controllers', ['ionic', 'datespot.services'])
 
-.controller('DashCtrl', function($scope) {})
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
+/*
+Controller for the Discover page
+*/
+
+.controller('DiscoverCtrl', function($scope) {
+
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+/*
+Controller for the Favorites page
+*/
+
+.controller('FavoritesCtrl', function($scope) {
+
 })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+/*
+Controller for the tab bar
+*/
+
+.controller('TabsCtrl', function($scope) {
+
+
+// .controller('DashCtrl', function($scope) {})
+
+// .controller('ChatsCtrl', function($scope, Chats) {
+//   $scope.chats = Chats.all();
+//   $scope.remove = function(chat) {
+//     Chats.remove(chat);
+//   }
+// })
+
+// .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+//   $scope.chat = Chats.get($stateParams.chatId);
+// })
+
+// .controller('AccountCtrl', function($scope) {
+//   $scope.settings = {
+//     enableFriends: true
+//   };
 });
